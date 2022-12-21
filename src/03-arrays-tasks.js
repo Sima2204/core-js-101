acc4b6d5745da2eb0b03bc36428bee2ed246d652
Sima2204 <vlad.simuta@gmail.com>
2022-12-21 22:58:27 +0200

    fix: arrays 1 left, date tasks started

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -474,8 +474,8 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n).fill(Array(n).fill()).map((el, i) => el.map((el2, i2) => (i === i2 ? 1 : 0)));
 }
 
 /**
@@ -566,8 +566,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat(Infinity);
 }
 
 
@@ -583,8 +583,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((accum, curr) => accum[curr], arr);
 }
 
 
@@ -606,10 +606,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const center = Math.floor((arr.length / 2));
+  return (arr.length % 2 === 0)
+    ? arr.slice(arr.length / 2).concat(arr.slice(0, (arr.length / 2)))
+    : arr.slice(center + 1).concat(arr[center], arr.slice(0, (center)));
 }
-
 
 module.exports = {
   findElement,
